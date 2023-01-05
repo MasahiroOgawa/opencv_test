97804edae4023dde79b529b1e09a3d55eec9a545
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-01-05 15:36:37 +0900

    add non square size filter

--- src/filter.cpp
+++ src/filter.cpp
@@ -7,10 +7,11 @@ int main() {
   // create input
   cv::Matx33f in_mat{1, 0, 0, 0, 0, 0, 0, 0, 0};
   //  cv::Matx33f filter{0, 1, 2, 3, 4, 5, 6, 7, 80};
-  cv::Matx22f filter{0, 1, 2, 3};
-  cv::Matx33f out_mat{};
+  //  cv::Matx22f filter{0, 1, 2, 3};
+  cv::Matx23f filter{10, 1, 2, 3, 4, 5};
 
   // process
+  cv::Matx33f out_mat{};
   int ddepth{-1}; // negative means the depth is the same with the source.
   cv::filter2D(in_mat, out_mat, ddepth, filter);
 
