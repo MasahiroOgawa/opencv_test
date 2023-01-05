2ed0e6b8538ee9eb8f4a60dc579f60e9c7f5f979
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-01-05 15:33:27 +0900

    add even size filter.

--- src/filter.cpp
+++ src/filter.cpp
@@ -4,9 +4,13 @@
 using namespace std;
 
 int main() {
+  // create input
   cv::Matx33f in_mat{1, 0, 0, 0, 0, 0, 0, 0, 0};
-  cv::Matx33f filter{0, 1, 2, 3, 4, 5, 6, 7, 80};
+  //  cv::Matx33f filter{0, 1, 2, 3, 4, 5, 6, 7, 80};
+  cv::Matx22f filter{0, 1, 2, 3};
   cv::Matx33f out_mat{};
+
+  // process
   int ddepth{-1}; // negative means the depth is the same with the source.
   cv::filter2D(in_mat, out_mat, ddepth, filter);
 
