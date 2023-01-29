91604930566960d5e1ab641e586fea396fe0db64
Masahiro Ogawa <masahiro.ogaw@gmail.com>
2023-01-30 07:57:36 +0900

    impolement mean stddev.

--- src/meanstddev.cpp
+++ src/meanstddev.cpp
@@ -19,12 +19,16 @@ int main() {
 
   // read images
   cv::Mat img;
+  cv::Scalar mean, stddev;
   for (const auto &imgname : imgnames) {
-    cout << imgname << endl;
-
     img = cv::imread(imgname, cv::IMREAD_GRAYSCALE);
 
+    cv::meanStdDev(img, mean, stddev);
+
     // display
+    cout << imgname << endl;
+    cout << "mean=" << mean << endl;
+    cout << "stddev=" << stddev << endl;
     cv::imshow("input image", img);
     cv::waitKey(0);
   }
